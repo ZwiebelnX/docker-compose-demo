231539b23dff283762089dd4f9ae3fa3f2924df7
Chen Sicong <sicong.chen@163.com>
2020-11-29 23:49:12 +0800

    [Chen Sicong] init project

---
+++ src/test/java/com/tw/csc/docker/composedemo/ComposeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.csc.docker.composedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposeDemoApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
